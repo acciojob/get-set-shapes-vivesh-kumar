9dc14cae37d8676f0f5f1dd14ac56788c3b279b6
Vivesh kumar <vivesh103@gmail.com>
2025-01-12 22:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		if(width<=0 ||height<=0){
+			get new error("width and height must be a positive integer.")
+		}
+		this._width;
+		this._height;
+	}
+	get width(){
+		return this._width;
+}
+	get height(){
+    return this._height;
+	}
+	getArea(){
+		return this._width*this._height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		supper(side,side);
+	}
+	getPerimeter(){
+		
+		return 4*this.width;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
